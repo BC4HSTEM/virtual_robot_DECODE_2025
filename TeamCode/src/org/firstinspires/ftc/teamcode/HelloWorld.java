b168e0c45a0a42b5c593eee1c21f72c85cab8253
ftc14169beta <ftc14169beta@gmail.com>
2025-10-09 19:55:51 -0400

    I started working with different data types. Like the Strings.

--- TeamCode/src/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -7,6 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
+        int teamNumber = 16072;
+        double motorSpeed = 0.5;
+        boolean touchSensorPressed = true;
+        String myName: "Your Name" ;
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Moter Speed", motorSpeed);
+        telemetry.addData("Touch sensor", touchSensorPressed);
         telemetry.addData("Hello", "World");
     }
 
